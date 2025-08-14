ef708fb74bdf3c07a7882778d5c6ffbf34e19cb2
Manolinhopng <rmanuelorrego@gmail.com>
2025-08-14 17:36:05 -0500

    Eliminar archivos rastreados que deberían ser ignorados

---
+++ src/main/kotlin/edu/unicatolica/kotlin/MySelf.kt
@@ -0,0 +1,22 @@
+package edu.unicatolica.kotlin
+
+fun main() {
+    val name: String = "Manuel Rendon"
+    val age: Int = 21
+    val program: String = "Tecnologia en desarrollo de software"
+
+    val interests = listOf(
+        "desarrollo web",
+        "apps mobiles",
+        "servicios en la nube",
+        "analisis de datos",
+        "ciberseguridad"
+    )
+
+    val basicMessage = "Me llamo $name, tengo $age años y estudio $program."
+
+    val interestsMessage = "Mis intereses son: ${interests.joinToString(", ")}."
+
+    println(basicMessage)
+    println(interestsMessage)
+}
